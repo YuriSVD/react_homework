27c7857175fea2426bcbc9b813caed7ac13ff637
yuri <yu.svyda@gmail.com>
2024-02-20 01:12:35 +0200

    update resolve 6

--- src/services/rickAndMorty.service.js
+++
@@ -1,8 +0,0 @@
-import {apiService} from "./api.service";
-import {urls} from "../configs";
-
-const rickAndMortyService = {
-    getAllEpisodes: () => apiService.get(urls.episodes),
-}
-
-export {rickAndMortyService};
